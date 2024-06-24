e629169cb4baa0a09a2d28e84312ce306eeb17e0
Antoine Marlot <amarlot@plastoria.be>
2024-06-24 18:33:39 +0200

    cron correction

--- scheduler/scripts/start.sh
+++ scheduler/scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -z ${ENABLE_DISPLAY_TIMER+x} ] && [ "$ENABLE_DISPLAY_TIMER" -eq "1" ]
 then
-  (crontab -l; echo "${DISPLAY_ON:-0 8 * * *} /usr/src/backlight_on.sh") | crontab -
-  (crontab -l; echo "${DISPLAY_OFF:-0 23 * * *} /usr/src/backlight_off.sh") | crontab -
+  (crontab -l; echo "${DISPLAY_ON:-0 7 * * *} /usr/src/display_on.sh") | crontab -
+  (crontab -l; echo "${DISPLAY_OFF:-0 18 * * *} /usr/src/display_off.sh") | crontab -
 fi
 
 crond -f
