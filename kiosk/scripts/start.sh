0e4e69b2d2d226266969ee553c96e728e74d7192
Antoine Marlot <amarlot@plastoria.be>
2024-06-24 13:10:23 +0200

    remote debug

---
+++ kiosk/scripts/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ ! -z $REMOTE_DEBUG ]] && [[ "$REMOTE_DEBUG" -eq "1" ]]
+then
+    echo "Forwarding remote debugging port 35174"
+    socat tcp-listen:35174,fork tcp:localhost:35173 &
+fi
